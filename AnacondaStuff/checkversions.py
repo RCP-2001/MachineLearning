49fcce96ffb1d10590e8480e84234079e5e4b09f
RCP-2001 <coalgolem.xd@gmail.com>
2023-01-12 18:09:32 -0800

    some training!

---
+++ AnacondaStuff/checkversions.py
@@ -0,0 +1,23 @@
+# checkversion.py
+# scipy
+import tensorflow as tf
+import sklearn
+import statsmodels
+import pandas
+import matplotlib
+import scipy
+import numpy
+
+print('scipy: %s' % scipy.__version__)
+# numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+print('sklearn: %s' % sklearn.__version__)
+# TensorFlow
+print('tensorflow: %s' % tf.__version__)
