c5e0de8fc9e96c74df7e32add5790cf256677626
rcp_2001 <coalgolem.xd@gmail.com>
2023-02-05 13:50:33 -0800

    cool

---
+++ LectureNotes/Gigachad-notes.py
@@ -0,0 +1,14 @@
+#notes but reject Jupyter, imbrace Python
+import numpy
+
+x = numpy.array([1,2,3,5])#sklearn
+x[1] = 1
+x[0] = '2'
+print(x)
+
+y = numpy.arange(0,10,0.5)
+print(y)
+
+z = numpy.ones((5,5))
+print(z)
+
